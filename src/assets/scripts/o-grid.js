ee29498f95e1e48070ac62f56249ed9cec0c4653
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2019-11-19 14:11:01 +0100

    add a grid layout list

---
+++ src/assets/scripts/o-grid.js
@@ -0,0 +1,30 @@
+const grid = document.querySelector('.o-grid');
+observeGrid(grid);
+
+function observeGrid(gridNode) {
+  // Feature detect ResizeObserver
+  if ('ResizeObserver' in window) {
+    // Get the min value from data-min="[min]"
+    const min = gridNode.dataset.min;
+    // Create a proxy element to measure and convert
+    // the `min` value (which might be em, rem, etc) to `px`
+    const test = document.createElement('div');
+    test.style.width = min;
+    gridNode.appendChild(test);
+    const minToPixels = test.offsetWidth;
+    gridNode.removeChild(test);
+
+    const ro = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        // Get the element's current dimensions
+        const cr = entry.contentRect;
+        // `true` if the container is wider than the minimum
+        const isWide = cr.width > minToPixels;
+        // toggle the class conditionally
+        gridNode.classList.toggle('aboveMin', isWide);
+      }
+    });
+
+    ro.observe(gridNode);
+  }
+}
