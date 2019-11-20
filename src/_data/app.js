22b52b934bb85b59d1ac22c2e997f5973e9b5a63
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2019-11-20 06:57:29 +0100

    app.url is set by process.env.URL it lets you know if you are on local or prod

---
+++ src/_data/app.js
@@ -0,0 +1,16 @@
+/* eslint-disable camelcase */
+module.exports = {
+  title: 'alienlebarge',
+  short_title: 'alb',
+  description: 'The personnal website of Cédric Aellen',
+  url: process.env.URL || '',
+  domain: 'alienlebarge.ch',
+  icon: '/images/app.ico',
+  author: {
+    name: 'Cédric Aellen',
+    url: 'https://alienlebarge.ch',
+    email: 'noreply@alienlebarge.ch',
+    avatar: 'https://www.gravatar.com/avatar/35449a5b09a740b214e4147d8893aee9?s=512'
+  },
+  feed_limit: 25
+};
