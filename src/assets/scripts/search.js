8383dc0a977036915e70d709de5b92f7b8d225d0
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2020-03-24 07:16:59 +0100

    add script

--- src/assets/scripts/search.js
+++ src/assets/scripts/search.js
@@ -2,9 +2,28 @@
 var btn = document.querySelector('#js-search-link');
 btn.addEventListener('click', function(event) {
 
-  // don't navigate to that page. Stay put.
-  event.preventDefault();
+    // don't navigate to that page. Stay put.
+    event.preventDefault();
 
-  // make search magic happen instead...
+    // make search magic happen instead...
+
+    // get the data
+    const searchIndex;
+    fetch('/search.json').then(function(response) {
+        return response.json();
+    }).then(function(response) {
+        searchIndex = response.search;
+    });
+
+    // look for matches in each item in the JSON
+    var results = [];
+    for(var item in searchIndex ) {
+        var found = searchIndex[item].text.indexOf(searchString);
+        if(found != -1 ) {
+            results.push(searchIndex[item])
+        }
+    }
+
+    // now display the items in our results array...
 
 }, false);
