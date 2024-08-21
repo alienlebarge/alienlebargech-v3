2964354ac66d8daedfeff1cb1bdeadbe07f52d0f
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2024-08-21 07:31:50 +0200

    Remove transformation of `bookmark-of`

--- src/bookmarks/bookmarks.11tydata.js
+++ src/bookmarks/bookmarks.11tydata.js
@@ -1,5 +1,6 @@
 module.exports = {
   eleventyComputed: {
-    bookmarkOf: data => data['bookmark-of'] || data['bookmark_of'],
+    // FIXME: 'bookmark-of' must also be transformed ex:`bookmarkOf: data => data['bookmark-of'] || data['bookmark_of'],`
+    bookmarkOf: data => data['bookmark_of'],
   },
 };
