326530faaea9ef2eaf30e21ea808abc083df160f
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2024-08-23 09:10:58 +0200

    handle both 'bookmark_of' and 'bookmark-of' in eleventyComputed
    Updated the eleventyComputed configuration to transform both 'bookmark_of' and 'bookmark-of' to 'bookmarkOf'.

--- src/bookmarks/bookmarks.11tydata.js
+++ src/bookmarks/bookmarks.11tydata.js
@@ -1,6 +1,5 @@
 module.exports = {
   eleventyComputed: {
-    // FIXME: 'bookmark-of' must also be transformed ex:`bookmarkOf: data => data['bookmark-of'] || data['bookmark_of'],`
-    bookmarkOf: data => data['bookmark_of'],
+    bookmarkOf: data => data['bookmark_of'] || data['bookmark-of'],
   },
 };
